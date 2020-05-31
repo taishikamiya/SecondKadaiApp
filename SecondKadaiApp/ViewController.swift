d423624654776b10e372cc39ddaa245dd401f46a
Taishi Kamiya <t.kamiya@edu.ciid.dk>
2020-05-31 16:40:13 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Taishi Kamiya on 2020/05/31.
+//  Copyright © 2020 taishi.kamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
